4f0f991f357991f768538b2e31a981589a336131
joao.fernandes <jbrunomf@outlook.com>
2025-10-27 18:58:06 -0400

    Add initial migration and configure database context

--- src/Data/AppDbContext.cs
+++ src/Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BugStore.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Customer> Customers { get; set; } = null!;
     public DbSet<Product> Products { get; set; } = null!;
